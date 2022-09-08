7276dfb67e15b02bcce3c84085c16d6d195130c9
BellowingButcher <bellowingbutcher@gmail.com>
2022-09-08 21:19:27 +0000

    This starts working on the functionality of the button

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ const story = document.querySelector('.story');
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
-}
 
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-randomize.addEventListener('click', result);
+document.getElementById('btn').addEventListener('click', result());
 
 function result() {
 
@@ -25,7 +24,7 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/14)+' stones';
     const temperature =  Math.round(5/(9(94-32)))+' centigrade';
-    replace('94 fahrenheit', document.querySelector(temperature));
+    storyText = storyText.replace('94 fahrenheit', document.querySelector(temperature));
     replace('300 pounds'), document.querySelector(weight);
   }
 
