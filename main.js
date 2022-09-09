4ae22591a5fd551ff2e920114dd20fd3fb239f30
BellowingButcher <bellowingbutcher@gmail.com>
2022-09-09 01:08:19 +0000

    Fixed the button functionality There were several bugs in the code that was fixed in one sitting. Not a fan of doing several fixes at once because i lost track of how many i did and what exactly i did. From now on I will start doing individual commits for fixes along the way.

--- main.js
+++ main.js
@@ -5,37 +5,36 @@ const story = document.querySelector('.story');
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
-
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+}
+const storyText = 'It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-document.getElementById('btn').addEventListener('click', () => {
-  alert ('textContent');
-};
+randomize.addEventListener('click', result);
 
 function result() {
-
+let newStory = storyText;
   if(customName.value !== '') {
     const name = customName.value;
-    storyText = storyText.replace('Bob', name);
+    newStory = newStory.replaceAll('Bob', name);
 
   }
 
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/14)+' stones';
-    const temperature =  Math.round(5/(9(94-32)))+' centigrade';
-    storyText = storyText.replace('94 fahrenheit', document.querySelector(temperature));
-    replace('300 pounds'), document.querySelector(weight);
+    const temperature =  Math.round((5/9)*(94-32))+' centigrade';
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
-  const newStory = newStory.replaceAll(('insertX', xItem), ('insertY', yItem), ('insertZ', zItem));
+ 
   const xItem = randomValueFromArray(insertX);
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replaceAll(':insertX:', xItem) .replaceAll(':insertY:', yItem) .replaceAll(':insertZ:', zItem);
 
-  
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+
 }
